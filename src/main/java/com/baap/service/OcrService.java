878ed10e3c18cb105411ad47daac35a214473fbf
Lokes2002 <kumarlok2002@gmail.com>
2025-11-26 19:25:56 +0530

    ho ja

--- src/main/java/com/baap/service/OcrService.java
+++ src/main/java/com/baap/service/OcrService.java
@@ -3,11 +3,8 @@ package com.baap.service;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
-
 import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import java.net.http.*;
 
 @Service
 public class OcrService {
